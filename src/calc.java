8e9e3e591638e754374785d62a4cd1ed7782944b
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-27 18:41:24 +0100

    up

--- src/calc.java
+++ src/calc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calculadora {
+public class calc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numUno, numDos;
